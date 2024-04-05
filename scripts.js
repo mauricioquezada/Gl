815b7d4bab97f85a3c97ab8bf0631193c5485b33
mauricioquezada <118624300+mauricioquezada@users.noreply.github.com>
2024-04-05 13:57:58 -0500

    puliendo detalles revisando las partes donde considero que hay fallas

---
+++ scripts.js
@@ -0,0 +1,17 @@
+document.getElementById('btnMensaje').addEventListener('click', function() {
+alert('lo que pasa en galdys se queda en gladys');
+});
+
+document.getElementById('btnAlerta').addEventListener('click', function() {
+    if (confirm('¿Estás seguro de mostrar esta alerta en la Página 2?')) { 
+alert('lo que pasa en gladys se queda en gladys');
+    }
+});
+    document.addEventListener("DOMContentLoaded", function() {
+        const form = document.getElementById("formulario");
+        form.addEventListener("submit", function(event) {
+            event.preventDefault();
+            window.location.href = "pgl.html";
+
+        });
+        });
