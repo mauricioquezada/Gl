6b19f9c23c9211bd70aa6cd1028827c8c1b2922c
mauricioquezada <118624300+mauricioquezada@users.noreply.github.com>
2024-04-05 14:44:00 -0500

    revisar estructura y detalles, arreglar el boton revisar por que no me da mas de 4 videos  mejorar la letra de tamano

--- scripts.js
+++ scripts.js
@@ -1,17 +1,22 @@
 document.getElementById('btnMensaje').addEventListener('click', function() {
-alert('lo que pasa en galdys se queda en gladys');
+alert('lo que pasa en galdys se queda en gladys');                            // Alerta desde la pagina 1
 });
 
 document.getElementById('btnAlerta').addEventListener('click', function() {
-    if (confirm('¿Estás seguro de mostrar esta alerta en la Página 2?')) { 
+    if (confirm('¿Estás seguro de mostrar esta alerta en la Página 2?')) {   // Alerta desde la pagina 1
 alert('lo que pasa en gladys se queda en gladys');
     }
 });
+
     document.addEventListener("DOMContentLoaded", function() {
         const form = document.getElementById("formulario");
         form.addEventListener("submit", function(event) {
             event.preventDefault();
-            window.location.href = "pgl.html";
+            window.location.href = "pgl.html";                   // la parte del formulario donde lo conecto 
+        });
 
-        });
-        });
+
+        function irAlInicio() {
+            window.location.href = "Pgl.html"; // Redirigir al inicio desde la pagina 2
+        }
+        });       
